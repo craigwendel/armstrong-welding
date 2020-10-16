6e9ed1fabebcc76ac4a27eab935f3b65e236c1c0
Craig Wendel <wendelcraig@gmail.com>
2020-10-16 17:21:29 -0500

    Add email support. Edit homepage and update theme

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { Divider } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -26,21 +27,54 @@ const useStyles = makeStyles(theme => ({
 
 export default function Home() {
   const classes = useStyles();
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+  const handleSubmit = () => {
+    console.log('THE STATE', state);
+    fetch('/api/send-email', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: state.name,
+        email: state.email,
+        phone: state.phone,
+        message: state.message,
+      }),
+    });
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="sm">
       <div className={classes.paper}>
         <img
           className={classes.info}
           src="/static/armstrong-logo.png"
           alt="armstrong welding logo"
         />
+        <Typography align="center" variant="h4">
+          Joseph Armstrong
+        </Typography>
+        <Typography variant="h6">512.525.3867</Typography>
         <Typography className={classes.info} variant="h6">
+          armstrongweldfab@gmail.com
+        </Typography>
+        <Typography className={classes.info} variant="subtitle">
           Our website is currently a work in progress but feel free to request a
           quote or more information below!
         </Typography>
-        <Typography variant="h5">Request a Quote / Information</Typography>
-        <form className={classes.form} noValidate>
+        <Typography variant="h6">Request a Quote / Information</Typography>
+        <div className={classes.form}>
           <TextField
             variant="outlined"
             margin="normal"
@@ -49,6 +83,8 @@ export default function Home() {
             id="name"
             label="Full Name"
             name="name"
+            value={state.name}
+            onChange={handleChange}
             autoFocus
           />
           <TextField
@@ -60,6 +96,8 @@ export default function Home() {
             id="email"
             label="Email Address"
             name="email"
+            value={state.email}
+            onChange={handleChange}
             autoComplete="email"
           />
           <TextField
@@ -71,29 +109,33 @@ export default function Home() {
             id="phone"
             label="Phone"
             name="phone"
+            value={state.phone}
+            onChange={handleChange}
             autoComplete="tel"
           />
           <TextField
             variant="outlined"
             margin="normal"
             fullWidth
-            id="comments"
-            label="Comments"
-            name="comments"
+            id="message"
+            label="Message"
+            name="message"
+            value={state.message}
+            onChange={handleChange}
             multiline
             rows={6}
-            defaultValue="Tell us more about your project..."
+            // defaultValue="Tell us more about your project..."
           />
           <Button
-            type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={handleSubmit}
           >
             Submit
           </Button>
-        </form>
+        </div>
       </div>
     </Container>
   );
---
+++ utils/sendEmail.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch';
+
+const SENDGRID_API = 'https://api.sendgrid.com/v3/mail/send';
+
+const sendEmail = async ({ name, email, phone, message }) => {
+  await fetch(SENDGRID_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [
+            {
+              email: 'wendelcraig@gmail.com',
+            },
+          ],
+          subject: `Quote / Information Inquiry from ${name}`,
+        },
+      ],
+      from: {
+        email,
+        name,
+      },
+      content: [
+        {
+          type: 'text/html',
+          value: `Name: ${name}
+                  Email: ${email}
+                  Phone: ${phone}
+                  Message: ${message}
+          `,
+        },
+      ],
+    }),
+  });
+};
+
+export { sendEmail };
