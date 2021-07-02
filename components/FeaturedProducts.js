2667fc5fd34f2258fe7841cd08ec381d4e464864
Craig Wendel <wendelcraig@gmail.com>
2021-07-02 15:26:55 -0500

    Add Featured Services, images

--- components/FeaturedProducts.js
+++ components/FeaturedProducts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -17,6 +18,11 @@ const useStyles = makeStyles({
     flexWrap: 'wrap',
     alignItems: 'center',
   },
+  homeContent: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+  },
   product: {
     marginRight: '1rem',
   },
@@ -29,12 +35,19 @@ const useStyles = makeStyles({
   },
   media: {
     height: 250,
+    width: '100%',
   },
   link: {
     cursor: 'pointer',
   },
   type: {
     margin: '1rem',
+  },
+  bullets: {
+    margin: '1rem 1rem 1rem 3rem',
+  },
+  services: {
+    marginLeft: '3rem',
   },
 });
 
@@ -54,60 +67,86 @@ export default function FeaturedProducts() {
         </Link>{' '}
         for a quote or more information below!
       </Typography>
-      <Typography className={classes.type} variant="h4" color="primary">
-        Featured Products
-      </Typography>
-      <div className={classes.container}>
-        {allProducts.map(product => (
-          <Card key={product.name} className={classes.root}>
-            {/* <CardActionArea> */}
-            <CardMedia
-              className={classes.media}
-              image={product.img}
-              title={product.name}
-            />
-            {/* </CardActionArea> */}
-            <CardContent>
-              <div className={classes.productPrice}>
-                <Typography
-                  className={classes.product}
-                  gutterBottom
-                  variant="h5"
-                  component="h2"
-                >
-                  {product.name}
-                </Typography>
-                <Typography
-                  color="primary"
-                  gutterBottom
-                  variant="h5"
-                  component="h2"
-                >
-                  {product.price}
-                </Typography>
-              </div>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {product.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                onClick={() => window.open(product.offerLink, '_blank')}
-                variant="contained"
-                size="small"
-                color="primary"
-              >
-                Make an Offer
-              </Button>
-              <Button onClick={() => window.open(product.secondaryLink, '_blank')}
-                variant="contained"
-                size="small"
-                color="secondary">
-                Visit Steinhauser's
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
+      <div className={classes.homeContent}>
+        <div>
+          <Typography className={classes.type} variant="h4" color="primary">
+            Featured Products
+          </Typography>
+          <div className={classes.container}>
+            {allProducts.map((product) => (
+              <Card key={product.name} className={classes.root}>
+                <Image
+                  src={product.img}
+                  width="350"
+                  height="250"
+                  alt={product.name}
+                />
+                <CardContent>
+                  <div className={classes.productPrice}>
+                    <Typography
+                      className={classes.product}
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                    >
+                      {product.name}
+                    </Typography>
+                    <Typography
+                      color="primary"
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                    >
+                      {product.price}
+                    </Typography>
+                  </div>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {product.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    onClick={() => window.open(product.offerLink, '_blank')}
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                  >
+                    Make an Offer
+                  </Button>
+                  <Button
+                    onClick={() => window.open(product.secondaryLink, '_blank')}
+                    variant="contained"
+                    size="small"
+                    color="secondary"
+                  >
+                    Visit Steinhauser's
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+          </div>
+        </div>
+        <div className={classes.services}>
+          <Typography className={classes.type} variant="h4" color="primary">
+            Featured Services
+          </Typography>
+          <Typography className={classes.type} variant="h5" color="primary">
+            Wrought Iron Work
+          </Typography>
+          <Typography className={classes.bullets} variant="h6" color="primary">
+            • Fences
+          </Typography>
+          <Typography className={classes.bullets} variant="h6" color="primary">
+            • Entry Ways
+          </Typography>
+          <Typography className={classes.bullets} variant="h6" color="primary">
+            • Staircases
+          </Typography>
+        </div>
       </div>
     </>
   );
